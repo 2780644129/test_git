a0da1806700774052a43c8769fb5d9ad9cecfb23
f2780644129 <2780644129@qq.com>
2024-11-02 19:21:45 +0800

    第一次提交

---
+++ src/com/coder/Test.java
@@ -0,0 +1,9 @@
+package com.coder;/**
+@author renguren
+@project IntelliJ IDEA
+@date 2024/11/2
+*/public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
